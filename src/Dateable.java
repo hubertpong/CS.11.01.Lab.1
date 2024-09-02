b2ac735368e33b85a6ef67ce1d69c690e2456809
hubert <hubertp2027@student.cis.edu.hk>
2024-09-02 09:09:25 +0800

    Completed

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in); //you must have this part to define the scanner
+
+        System.out.print("Enter your age: "); // prints the msg out
+        int age = scanner.nextInt(); //this part reads the value the user input and then stores it as "age"
+
+        int minDateAge = (age / 2) + 7; // calculation part of the code and stores it as "minDateAge"
+        System.out.println(age + "-year olds should date somebody who is at least " + minDateAge + " years old."); // then we have both "age" and "minDateAge" identified then we can fill the sentence in
+
+        scanner.close(); // closes the scanner
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt(); // Read the user's age
+
+
+        int minPages = 100 - age;
+        System.out.println(age + "-year olds should read at least " + minPages + " pages before giving up on a book.");
+
+        scanner.close(); // Close the scanner
     }
 }
